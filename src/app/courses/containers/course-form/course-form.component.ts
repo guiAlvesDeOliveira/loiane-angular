d33a73e5bfabd4634cd7063a41448488ea4ba310
J34993R <guialves.oliveira1234@gmail.com>
2023-09-25 11:21:26 -0300

    Aula 49

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {NonNullableFormBuilder, Validators} from "@angular/forms";
+import {FormGroup, NonNullableFormBuilder, Validators} from "@angular/forms";
 import {CoursesService} from "../courses/services/courses.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {Location} from "@angular/common";
 import {ActivatedRoute} from "@angular/router";
 import {Course} from "../courses/model/course";
+import {Lesson} from "../courses/model/lesson";
 
 @Component({
   selector: 'app-course-form',
@@ -13,14 +14,7 @@ import {Course} from "../courses/model/course";
 })
 export class CourseFormComponent implements OnInit {
 
-  form = this.formBuilder.group({
-    _id: [''],
-    name: ['', [Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(100)
-    ]],
-    category: ['', [Validators.required]]
-  });
+  form!: FormGroup;
 
   constructor(private formBuilder: NonNullableFormBuilder,
               private service: CoursesService,
@@ -32,10 +26,35 @@ export class CourseFormComponent implements OnInit {
 
   ngOnInit(): void {
     const course: Course = this.route.snapshot.data['course'];
-    this.form.setValue({
-      _id: course._id,
-      name: course.name,
-      category: course.category
+    this.form = this.formBuilder.group({
+      _id: [course._id],
+      name: [course.name, [Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100)
+      ]],
+      category: [course.category, [Validators.required]],
+      lessons: this.formBuilder.array(this.retriveLessons(course))
+    });
+    console.log(this.form);
+    console.log(this.form.value);
+
+  }
+
+  private retriveLessons(course: Course) {
+    const lessons = []
+    if (course?.lessons) {
+      course.lessons.forEach(lesson => lessons.push(this.createLesson(lesson)));
+    } else {
+      lessons.push(this.createLesson())
+    }
+    return lessons;
+  }
+
+  private createLesson(lesson: Lesson = {id: '', name: '', youtubeUrl: ''}) {
+    return this.formBuilder.group({
+      id: [lesson.id],
+      name: [lesson.name],
+      youtubeUrl: [lesson.youtubeUrl]
     })
   }
 
@@ -59,14 +78,14 @@ export class CourseFormComponent implements OnInit {
 
   getErrorMessage(fieldName: string) {
     const field = this.form.get(fieldName);
-    if (field?.hasError('required')){
+    if (field?.hasError('required')) {
       return 'Campo Obrigatório';
     }
-    if (field?.hasError('minlength')){
+    if (field?.hasError('minlength')) {
       const requiredLength = field?.errors ? field.errors['minlength']['requiredLength'] : 5;
       return `Tamanho minimo precisa ser de ${requiredLength} caracteres`;
     }
-    if (field?.hasError('maxlength')){
+    if (field?.hasError('maxlength')) {
       const requiredLength = field?.errors ? field.errors['maxlength']['requiredLength'] : 200;
       return `Tamanho máximo de ${requiredLength} caracteres`;
     }
